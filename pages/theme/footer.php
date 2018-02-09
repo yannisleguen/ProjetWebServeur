4176572fb1b0420708a9cbe0bed987111f1a109c
yannisleguen <34504956+yannisleguen@users.noreply.github.com>
2018-02-09 17:51:21 +0100

    Add web static site
    Il faut (Alex & Nico)
    
    index: 
    Permettre de :connecter le formulaire de recherche à la base de donnée
    load les 5 dernières publi, load les 5 categ de travaux les plus utilisés, load les 5 destinations les plus choisies
    
    subscribe.php = permettre d'ajouter une personne de manière sécurisée (PRIORITÉ)
    login.php: permettre la connexion

---
+++ pages/theme/footer.php
@@ -0,0 +1,16 @@
+<div id="footerwrap">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-4">
+					<p><b>WEB DESIGNER, DEVELOPER & GAME ADDICT</b></p>
+				</div>
+			
+				<div class="col-lg-4">
+					<p>Parisien 🤙.</p>
+				</div>
+				<div class="col-lg-4">
+					<p>yannis.le-guen@hotmail.fr</p>
+				</div>
+			</div>
+		</div>
+	</div><! --/footerwrap -->
