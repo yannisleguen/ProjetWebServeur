9f13f1f35ad0b59838a2b0d20f04eb4adf1e5f90
yannisleguen <34504956+yannisleguen@users.noreply.github.com>
2018-02-23 12:27:37 +0100

    Add files via upload
    On peut ajouter une annonce

--- pages/announce.php
+++ pages/announce.php
@@ -1,28 +1,44 @@
 
-<form name="addAnnounce" action="#" method="POST">
+<form name="addAnnounce" action="addAnnounce.php" method="POST">
 <p>Announce Name :</p>
-        <input class="form-control" type="text" placeholder="Name"  id="name"> 
-<p>Work to do :</p>
-        <input class="form-control" type="text" placeholder="Work Name" id="work_name"> 
+        <input class="form-control" type="text" placeholder="Name"  name="name"> 
+
 <div>
       <p>Select a job category : </p>
         <select name="categ_job" class="form-control">
-          <option value="1">Will be load from db</option>
-          <option value="2">Will be load from db</option>
-          <option value="2">...</option>
+          <?php 
+            require("../scripts/connect.php");
+            $db = db_connect();
+            $query = "SELECT id_Category, name_Category FROM category";
+            $statement = $db->prepare($query);
+            $statement->execute();
+            while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+              $id = $row["id_Category"];
+              $categ = $row["name_Category"];
+              echo "<option value=".$id.">".$categ."</option>";
+            }
+            ?>
+          
         </select>
 </div> 
 
+<p>Adress: </p>
+        <input class="form-control" type="text" placeholder="Adress" name="adress"> 
+
 <p>City :</p>
-        <input class="form-control" type="text" placeholder="City" id="city"> 
+        <input class="form-control" type="text" placeholder="City" name="city"> 
 <p>Country :</p>
         <input class="form-control" type="text" placeholder="Country" id="country"> 
 
 <p>Photo :</p>
-	<input type="file" name="photo1" id="photoUsr"/>
+	  <input type="file" name="photo1" id="photoUsr"/>
     <input type="file" name="photo2" id="photoUsr"/>
     <input type="file" name="photo3" id="photoUsr"/>
 <p>Description :</p>
 	<textarea name="description" id="descriptionUsr"></textarea>
+
+<p>Chose your dates : </p>
+      <input class="form-control formdate" type="date" value="Check-in" name="check-in">
+      <input class="form-control formdate" type="date" value="Check-out" name="check-out">
 
 <input type="submit" value="Subscribe">
