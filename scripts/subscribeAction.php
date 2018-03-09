db2959192c6eac4fe64c2d660d8ac9329dac79e1
Alexdesc <35339436+Alexdesc@users.noreply.github.com>
2018-03-09 12:06:21 +0100

    Add files via upload

--- scripts/subscribeAction.php
+++ scripts/subscribeAction.php
@@ -16,8 +16,13 @@ $description= $_POST['description'];
 
 
 if($db) {
-	$query = "INSERT INTO user(pseudo_User,name_User,first_Name_User,tel_User,email_User,sexe_User,birthsday,passwrd_User) VALUES(:user,:nom,:prenom,:tel,:email,:sexe,:birth,:psw)";
-	$statement = $db->prepare($query);
+	 $query = "INSERT INTO `user` (`id_User`, `name_User`, `first_Name_User`, `tel_User`, `email_User`, `sexe_User`, `birthsday`, `passwrd_User`, `Description du User`) VALUES (NULL, '".$name."', '".$firstName."', '".$phone."', '".$email."', '".$sexe."', '".$birthdate."', '".$password."', '".$description."');";
+	  $statement = $db->prepare($query);
+      $statement->execute();
+	/*$db->exec("INSERT INTO user(pseudo_User,name_User,first_Name_User,birthsday,tel_User,email_User,passwrd_User,sexe_User) VALUES('$user','$name','$firstName','$birthdate','$phone','$email','$password','$sexe')");
+
+	/*$sql = "INSERT INTO user(pseudo_User,name_User,first_Name_User,tel_User,email_User,sexe_User,birthsday,passwrd_User) VALUES(:user,:nom,:prenom,:tel,:email,:sexe,:birth,:psw)";
+	$statement = $db->prepare($sql);
 	//On bind toutes les values avec les valeurs des variables
 	$statement->bindValue(':user', $user);
 	$statement->bindValue(':prenom', $firstName);
@@ -27,11 +32,7 @@ if($db) {
 	$statement->bindValue(':sexe', $sexe);
 	$statement->bindValue(':birth', $birthdate);
 	$statement->bindValue(':psw', $password);
-	$statement->execute();
-
-	if($statement = true){
-		echo "test réussi";
-	}
+	$statement->execute();*/
 }
 
 ?>
