20513775e160994d835c9d6dd8d0fddcf409d353
yannisleguen <34504956+yannisleguen@users.noreply.github.com>
2018-03-09 10:02:30 +0100

    epvojer
    vermopv

---
+++ addAnnounce.php
@@ -0,0 +1,13 @@
+<?php 
+$name = $_POST['name'];
+
+$categ = $_POST['categ_job'];
+$adress = $_POST['adress'];
+$city = $_POST['city'];
+$country = $_POST['country'];
+$pictures = array ($_POST["photo1"],$_POST["photo2"],$_POST["photo3"]);
+$description = $_POST['description'];
+$dateIn = $_POST['check-in'];
+$dateOut = $_POST['check-out'];
+
+?>
